2bf4b9e29c278406c82c8af11142ff83030749da
bsavanth <bsavanth@uark.edu>
2022-07-13 00:51:36 -0500

    CateogryID Fix

--- src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -2,6 +2,12 @@ package com.luv2code.ecommerce.config;
 
 import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.entity.ProductCategory;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -11,6 +17,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+//    @Autowired
+//    public MyDataRestConfig(EntityManager theEntityManager)
+//    {
+//        this.entityManager = theEntityManager;
+//    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
--- src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -29,16 +29,16 @@ public class Product {
     @Column(name = "sku") private String sku;
     @Column(name = "name") private String name;
     @Column(name = "description") private String description;
-    @Column(name = "unit_price") private BigDecimal unit_price;
-    @Column(name = "image_url") private String image_url;
+    @Column(name = "unit_price") private BigDecimal unitPrice;
+    @Column(name = "image_url") private String imageUrl;
     @Column(name = "active") private boolean active;
-    @Column(name = "units_in_stock") private int units_in_stock;
+    @Column(name = "units_in_stock") private int unitsInStock;
 
     @CreationTimestamp
-    @Column(name = "date_created") private Date date_created;
+    @Column(name = "date_created") private Date dateCreated;
 
     @UpdateTimestamp
-    @Column(name = "last_updated") private Date last_updated;
+    @Column(name = "last_updated") private Date lastUpdated;
 
     @ManyToOne @JoinColumn(name = "category_id", nullable = false)
     private ProductCategory category;
