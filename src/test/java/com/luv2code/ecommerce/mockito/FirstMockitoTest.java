f7269acb71bc8feaa2447735a749d58013842029
bsavanth <bsavanth@uark.edu>
2022-08-12 03:14:19 -0500

    added readMe

---
+++ src/test/java/com/luv2code/ecommerce/mockito/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+package com.luv2code.ecommerce.mockito;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FirstMockitoTest {
+
+    @Test
+    public void test()
+    {
+        assertTrue(true);
+    }
+}
