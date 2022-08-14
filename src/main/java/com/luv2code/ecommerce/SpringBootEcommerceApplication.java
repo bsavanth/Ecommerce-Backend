ea42c55623a1c8912c7be2c8aff379d77734b3c0
bsavanth <bsavanth@uark.edu>
2022-08-13 22:06:23 -0500

    swagger support

--- src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -13,7 +13,7 @@ public class SpringBootEcommerceApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringBootEcommerceApplication.class, args);
+        //SpringApplication.run(SpringBootEcommerceApplication.class, args);
         new SpringApplicationBuilder(SpringBootEcommerceApplication.class).web(WebApplicationType.NONE).run(args);
     }
 
